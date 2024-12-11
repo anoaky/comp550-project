97a82c19445039f38ae3f81c2ec126b3c6a0629a
Anokhi Suresh <17suresa@gmail.com>
2024-12-11 18:51:25 -0500

    rework

--- t5.py
+++ t5.py
@@ -11,10 +11,10 @@ from tqdm import tqdm
 from trainer import prep_dataset
 from bart import SBFDataset
 
-def setup(world_size):
+def setup(rank, world_size):
     os.environ['MASTER_ADDRESS'] = 'localhost'
     os.environ['MASTER_PORT'] = '12345'
-    dist.init_process_group('nccl', init_method=f'file:///tmp/com', world_size=world_size)
+    dist.init_process_group('nccl', rank=rank, world_size=world_size)
     
 def cleanup():
     dist.destroy_process_group()
@@ -38,36 +38,9 @@ def shard_model(rank, model):
     sharded_model = FullyShardedDataParallel(model, cpu_offload=CPUOffload(offload_params=True))
     optim = torch.optim.Adam(sharded_model.parameters(), lr=1e-5)
     return sharded_model, optim
-
-def fit(rank, world_size, model, max_epochs, train_loader: DataLoader):
-    setup(world_size)
-    with tqdm() as t:
-        model, optimizer = shard_model(rank, model)
-        for epoch in range(max_epochs):
-            train_loader.sampler.set_epoch(epoch)
-            t.reset(total=len(train_loader))
-            t.set_description(f'Training epoch {epoch}')
-            for idx, batch in enumerate(train_loader):
-                optimizer.zero_grad()
-                loss = model(idx, *batch)
-                loss.backward()
-                optimizer.step()
-                t.set_postfix(loss=loss.item())
-                t.update()
                 
-class FSDPTrainer:
-    def __init__(self, world_size):
-        self.world_size = world_size
-    def fit(self, model, max_epochs, train_loader):
-        mp.spawn(fit,
-                 args=(self.world_size, model, max_epochs, train_loader),
-                 nprocs=self.world_size,
-                 join=True)
-        
-if __name__ == '__main__':
-    world_size = torch.cuda.device_count()
-    print(f'WORLD_SIZE: {world_size}')
-    setup(world_size)
+def main(rank, world_size):
+    setup(rank, world_size)
     tokenizer = T5Tokenizer.from_pretrained('google-t5/t5-base')
     train_set = load_dataset('allenai/social_bias_frames', split='train', trust_remote_code=True)
     train_set = prep_dataset(train_set)
@@ -77,7 +50,33 @@ if __name__ == '__main__':
     
     config = T5Config(num_labels=8)
     model = T5Bias(config)
+    model, optimizer = shard_model(rank, model)
     
     trainer = FSDPTrainer(world_size)
-    trainer.fit(model, 1, train_loader)
+    trainer.fit(model, optimizer, 1, train_loader)
+                
+class FSDPTrainer:
+    def __init__(self, world_size):
+        self.world_size = world_size
+    def fit(self, model, optimizer, max_epochs, train_loader):
+        with tqdm() as t:
+            for epoch in range(max_epochs):
+                train_loader.sampler.set_epoch(epoch)
+                t.reset(total=len(train_loader))
+                t.set_description(f'Training epoch {epoch}')
+                for idx, batch in enumerate(train_loader):
+                    optimizer.zero_grad()
+                    loss = model(idx, *batch)
+                    loss.backward()
+                    optimizer.step()
+                    t.set_postfix(loss=loss.item())
+                    t.update()
+        
+if __name__ == '__main__':
+    world_size = torch.cuda.device_count()
+    print(f'WORLD_SIZE: {world_size}')
+    mp.spawn(main,
+             args=(world_size,),
+             nprocs=world_size,
+             join=True)
     cleanup()
