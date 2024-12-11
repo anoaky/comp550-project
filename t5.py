52a9dd92f93c02a5e2d4b5e83a9b5384f7f20e48
Anokhi Suresh <17suresa@gmail.com>
2024-12-11 18:51:54 -0500

    fix env variable

--- t5.py
+++ t5.py
@@ -12,7 +12,7 @@ from trainer import prep_dataset
 from bart import SBFDataset
 
 def setup(rank, world_size):
-    os.environ['MASTER_ADDRESS'] = 'localhost'
+    os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_PORT'] = '12345'
     dist.init_process_group('nccl', rank=rank, world_size=world_size)
     
