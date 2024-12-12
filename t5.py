fd01cf2a0411c483cc3f7b6486173c09916e0e0c
Anokhi Suresh <17suresa@gmail.com>
2024-12-11 19:07:14 -0500

    adjusted init_method to file

--- t5.py
+++ t5.py
@@ -14,8 +14,8 @@ from bart import SBFDataset
 
 def setup(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12345'
-    dist.init_process_group('nccl', rank=rank, world_size=world_size)
+    os.environ['MASTER_PORT'] = '45355'
+    dist.init_process_group('nccl', init_method='file:///tmp/sharedfile', rank=rank, world_size=world_size)
     
 def cleanup():
     dist.destroy_process_group()
