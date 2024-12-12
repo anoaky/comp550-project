82764b7dca6ae9da8f56f1b344bade6d6b02af84
Anokhi Suresh <17suresa@gmail.com>
2024-12-12 10:03:20 -0500

    lightning fabric

--- t5.py
+++ t5.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 import comet_ml
 import torch
 import torch.distributed as dist
@@ -8,112 +9,97 @@ from torch.utils.data import Dataset, DataLoader, DistributedSampler
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.distributed.fsdp import FullyShardedDataParallel, CPUOffload
 import torch.multiprocessing as mp
-from transformers import T5Tokenizer, T5Config, T5Model
+from transformers import T5Tokenizer, T5Config, AutoModel
 import os
 from tqdm import tqdm
-from trainer import prep_dataset
+from trainer import prep_dataset, FabricTrainer, CometCallback, FabricSummary
 from bart import SBFDataset
+import lightning as L
 
-def setup(rank, world_size):
-    os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '45355'
-    dist.init_process_group('nccl', init_method='file:///tmp/sharedfile', rank=rank, world_size=world_size)
     
-def cleanup():
-    dist.destroy_process_group()
-    
-class T5Bias(nn.Module):
+class T5Bias(L.LightningModule):
     def __init__(self):
         super().__init__()
-        self.t5 = T5Model.from_pretrained('t5-11b', ignore_mismatched_sizes=True)
+        self.t5 = AutoModel.from_pretrained('t5-11b', ignore_mismatched_sizes=True)
         self.fc1 = nn.Linear(1024, 512)
         self.fc2 = nn.Linear(512, 64)
         self.fc3 = nn.Linear(64, 8)
+        self.t5.train()
     
-    def forward(self, input_ids, attention_mask):
+    def forward(self, input_ids, attention_mask, /):
         _, x = self.t5(input_ids, attention_mask=attention_mask, return_dict=False)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
     
-def prepare_model(rank):
-    model = T5Bias()
-    model = model.to(rank)
-    model = DDP(model,
-                device_ids=[rank],
-                output_device=rank,
-                find_unused_parameters=True)
-    return model
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=1e-5)
     
-def get_loaders_ddp():
-    tokenizer = T5Tokenizer.from_pretrained('t5-base')
-    train_set = load_dataset('allenai/social_bias_frames', split='train', trust_remote_code=True)
-    train_set = prep_dataset(train_set)
-    train_set = SBFDataset(train_set, tokenizer)
-    train_sampler = DistributedSampler(train_set, drop_last=True)
-    train_loader = DataLoader(train_set, batch_size=32, shuffle=False, sampler=train_sampler)
+    def training_step(self, input_ids, attention_mask, y, /):
+        yh = self.forward(input_ids, attention_mask)
+        train_loss = F.binary_cross_entropy_with_logits(yh, y)
+        return train_loss
     
-    val_set = load_dataset('allenai/social_bias_frames', split='validation', trust_remote_code=True)
-    val_set = prep_dataset(val_set)
-    val_set = SBFDataset(val_set, tokenizer)
-    val_sampler = DistributedSampler(val_set, drop_last=True, shuffle=False)
-    val_loader = DataLoader(val_set, batch_size=32, shuffle=False, sampler=val_sampler)
+    def validation_step(self, input_ids, attention_mask, y):
+        yh = self.forward(input_ids, attention_mask)
+        val_loss = F.binary_cross_entropy_with_logits(yh, y)
+        yh = torch.argmax(yh, dim=1)
+        val_acc = torch.sum(y == yh).item() / (y.shape[0] * 1.0)
+        return val_loss, val_acc
     
-    return train_loader, val_loader
+    def train_dataloader(self, tokenizer, **kwargs):
+        train_set = load_dataset('allenai/social_bias_frames', split='train', trust_remote_code=True)
+        train_set = prep_dataset(train_set)
+        train_set = SBFDataset(train_set, tokenizer)
+        train_loader = DataLoader(train_set, shuffle=True, **kwargs)
+        return train_loader
+    
+    def val_dataloader(self, tokenizer, **kwargs):
+        val_set = load_dataset('allenai/social_bias_frames', split='validation', trust_remote_code=True)
+        val_set = prep_dataset(val_set)
+        val_set = SBFDataset(val_set, tokenizer)
+        val_loader = DataLoader(val_set, shuffle=False, **kwargs)
+        return val_loader
+    
                 
-def main(rank, world_size, experiment_key):
-    setup(rank, world_size)
-    torch.cuda.set_device(rank)
-    device = torch.device(f'cuda:{rank}')
-    experiment = comet_ml.start(api_key=os.environ['COMET_API_KEY'],
-                                experiment_key=experiment_key)
-    experiment.disable_mp()
-    train_loader, val_loader = get_loaders_ddp()
+def main(args):
+    expconfig = comet_ml.ExperimentConfig(disabled=True,
+                                          name=args.experiment_name)
+    comet_cb = CometCallback(prefix='t5-11B', 
+                             api_key=os.environ['COMET_API_KEY'], 
+                             workspace='anoaky',
+                             project_name='comp-550-project',
+                             experiment_config=expconfig)
+    fabric_summary = FabricSummary()
+    dataloader_kwargs = {
+        'batch_size': args.batch_size,
+        'num_workers': args.num_workers,
+        'drop_last': True,
+        'pin_memory': True,
+        'prefetch_factor': 8
+    }
     
-    model = prepare_model(rank)
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
-    t = tqdm()
-    for epoch in range(10):
-        train_loader.sampler.set_epoch(epoch)
-        t.reset(total=len(train_loader))
-        t.set_description(f'Training epoch {epoch}')
-        with experiment.train():
-            for idx, batch in enumerate(train_loader):
-                optimizer.zero_grad()
-                input_ids = batch[0].to(rank)
-                attention_mask = batch[1].to(rank)
-                yh = batch[2].to(rank)
-                y = model(input_ids, attention_mask)
-                loss = F.binary_cross_entropy_with_logits(y, yh)
-                loss.backward()
-                optimizer.step()
-                if idx % 10 == 9:
-                    gloss = [torch.zeros(1, dtype=torch.float32, device=device) for _ in range(world_size)]
-                    dist.all_gather(gloss, loss)
-                    loss = torch.mean(gloss)
-                    experiment.log_metrics({f'loss': loss.item()}, 
-                                           prefix='t5-11b',
-                                           step=idx+1,
-                                           epoch=epoch)
-                    t.set_postfix(loss=loss.item())
-                t.update()
-    t.close()
+    model = T5Bias()
+    torch.compile(model)
+    tokenizer = T5Tokenizer.from_pretrained('t5-base')
+    train_loader = model.train_dataloader(tokenizer, **dataloader_kwargs)
+    val_loader = model.val_dataloader(tokenizer, **dataloader_kwargs)
+    fabric = L.Fabric(callbacks=[comet_cb, fabric_summary],
+                      loggers=[])
+    trainer = FabricTrainer(fabric,
+                            max_epochs=args.max_epochs,
+                            log_every=args.log_every)
+    trainer.fit(model, train_loader=train_loader, val_loader=val_loader)
+        
         
 if __name__ == '__main__':
     torch.set_float32_matmul_precision('medium')
-    expconfig = comet_ml.ExperimentConfig(name='t5-11b-32')
-    experiment = comet_ml.start(api_key=os.environ['COMET_API_KEY'],
-                                workspace='anoaky',
-                                project_name='comp-550-project',
-                                experiment_config=expconfig)
-    experiment.disable_mp()
-    experiment.log_parameters({'batch_size': 32, 'max_epochs': 10})
-    experiment.end()
-    world_size = torch.cuda.device_count()
-    print(f'WORLD_SIZE: {world_size}')
-    mp.spawn(main,
-             args=(world_size,experiment.get_key()),
-             nprocs=world_size,
-             join=True)
-    cleanup()
+    parser = ArgumentParser()
+    parser.add_argument('--experiment_name', required=True, type=str)
+    parser.add_argument('--max_epochs', default=10, type=int)
+    parser.add_argument('--batch_size', default=32, type=int)
+    parser.add_argument('--num_workers', default=16, type=int)
+    parser.add_argument('--log_every', default=50, type=int)
+    args = parser.parse_args()
+    main(args)
--- trainer.py
+++ trainer.py
@@ -5,9 +5,11 @@ import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 from datasets import load_dataset
 from tqdm import tqdm
+import lightning as L
+from lightning.pytorch.callbacks import ModelSummary
+from lightning.pytorch.utilities.model_summary import summarize
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
 
 def prep_dataset(ds: Dataset) -> Dataset:
     def replace_empty(row):
@@ -16,6 +18,78 @@ def prep_dataset(ds: Dataset) -> Dataset:
         return row
     return ds.select_columns(['post', 'targetCategory']).map(replace_empty).class_encode_column('targetCategory')
 
+class FabricSummary:
+    def __init__(self, max_depth: int=1):
+        self.max_depth = max_depth
+        
+    def print_summary(self, module: L.LightningModule):
+        model_summary = summarize(module, max_depth=self.max_depth)
+        ModelSummary.summarize(model_summary._get_summary_data(),
+                               model_summary.total_parameters,
+                               model_summary.trainable_parameters,
+                               model_summary.model_size,
+                               model_summary.total_training_modes)
+
+class CometCallback:
+    def __init__(self, *, prefix, **experiment_kwargs):
+        self.experiment = comet_ml.start(**experiment_kwargs)
+        self.experiment.disable_mp()
+        self.prefix = prefix
+        
+    def log_metrics(self, *, epoch, step=None, **kwargs):
+        self.experiment.log_metrics(kwargs, prefix=self.prefix, step=step, epoch=epoch)
+        
+    def on_fit_end(self):
+        self.experiment.end()
+
+class FabricTrainer:
+    def __init__(self, fabric: L.Fabric, *, max_epochs: int, log_every: int):
+        self.fabric = fabric
+        self.max_epochs = max_epochs
+        self.log_every = log_every
+        
+    def fit(self, model: L.LightningModule, *, train_loader: DataLoader, val_loader: DataLoader):
+        self.fabric.call("print_summary")
+        self.fabric.launch()
+        optimizer = model.configure_optimizers()
+        model, optimizer = self.fabric.setup(model, optimizer)
+        [train_loader, val_loader] = self.fabric.setup_dataloaders(train_loader, val_loader)
+        best_accuracy = 0.0
+        t = tqdm()
+        for epoch in range(self.max_epochs):
+            t.reset(total=len(train_loader))
+            t.set_description(f'Training epoch {epoch}')
+            model.train()
+            for idx, batch in enumerate(train_loader):
+                optimizer.zero_grad()
+                loss = model.training_step(*batch)
+                self.fabric.backward(loss)
+                optimizer.step()
+                
+                if idx % self.log_every == self.log_every - 1:
+                    all_loss = self.fabric.all_reduce(loss)
+                    if self.fabric.is_global_zero:
+                        self.fabric.call("log_metrics", epoch=epoch, step=idx, train_loss=all_loss.item())
+                    t.set_postfix(loss=all_loss.item())
+                t.update()
+            # validation
+            t.reset(total=len(val_loader))
+            t.set_description(f'Validation epoch {epoch}')
+            model.eval()
+            proc_accuracy = torch.tensor(0, device=self.fabric.device)
+            with torch.no_grad():
+                for idx, batch in enumerate(val_loader):
+                    loss, accuracy = model.validation_step(*batch)
+                    proc_accuracy = proc_accuracy + accuracy
+                    t.update()
+            proc_accuracy = proc_accuracy / len(val_loader)
+            accuracy = self.fabric.all_reduce(proc_accuracy).item()
+            if accuracy > best_accuracy:
+                best_accuracy = accuracy
+                if self.fabric.is_global_zero:
+                    self.fabric.call("log_metrics", epoch=epoch, val_acc=accuracy)
+        t.close()
+        self.fabric.call("on_fit_end")
 
 class Trainer:
     def __init__(self, experiment: comet_ml.Experiment, max_epochs, log_every):
