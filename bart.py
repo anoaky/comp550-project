936af1e5b90dbf751449316dd9ce99fc60e80cc6
Anokhi Suresh <17suresa@gmail.com>
2024-12-15 14:09:28 -0500

    switch to huggingface trainer

--- bart.py
+++ bart.py
@@ -1,64 +1,55 @@
-import comet_ml
 import torch
 import torch.optim
-import huggingface_hub as hf
-from transformers import BartTokenizer
-from sbf_common import SBFModel, SBFTrainer
+from transformers import BartTokenizer, BartForSequenceClassification, Trainer, TrainingArguments
 from argparse import ArgumentParser
-        
-class SBFBart(SBFModel):
-    def __init__(self, label_key: str):
-        super().__init__(model_path='facebook/bart-large-mnli',
-                         base_name=f'sbf-bart-{label_key}',)
-        self.label_key = label_key
+from sbf_common import get_dataset, cls_loss, cls_metrics
+import os
 
 def main(args):
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    expconf = comet_ml.ExperimentConfig(#disabled=True,
-                                        name=args.experiment_name,
-                                        parse_args=False)
-    experiment = comet_ml.start(workspace='anoaky',
-                                project_name='comp-550-project',
-                                experiment_config=expconf)
-    experiment.disable_mp()
-    model = SBFBart(args.problem)
-    trainer = SBFTrainer(max_epochs=args.max_epochs,
-                         step_every=args.step_every,
-                         experiment=experiment)
+    model = BartForSequenceClassification.from_pretrained('facebook/bart-large-mnli',
+                                                          num_labels=1,
+                                                          ignore_mismatched_sizes=True,)
     tokenizer = BartTokenizer.from_pretrained('facebook/bart-large')
-    dataloader_kwargs = {
-        'batch_size': args.batch_size,
-        'num_workers': args.num_workers,
-        'drop_last': True,
-        'prefetch_factor': 2,
-    }
-    train_loader, val_loader, test_loader = model.dataloaders(tokenizer, **dataloader_kwargs)
-    try:
-        trainer.fit(model,
-                    device,
-                    train_loader=train_loader,
-                    val_loader=val_loader,)
-    except Exception as e:
-        experiment_key = experiment.get_key()
-        experiment.send_notification(args.experiment_name,
-                                     status='failed')
-        experiment.end()
-        comet_ml.APIExperiment(previous_experiment=experiment_key).archive()
-        raise e
-    experiment.end()
-    experiment.send_notification(args.experiment_name,
-                                 status='finished')
-    model.push_to_hub(f'anoaky/{model.base_name}',
-                      branch=args.branch_name)
+    model_id = f'anoaky/sbf-bart-{args.problem}'
+    feature = f'{args.problem}YN'
+    out_dir = f'{os.environ['SLURM_TMPDIR']}/{model_id}'
+    targs = TrainingArguments(output_dir=out_dir,
+                              run_name=f'bart-{args.problem}',
+                              do_train=True,
+                              do_eval=True,
+                              per_device_train_batch_size=args.batch_size,
+                              per_device_eval_batch_size=args.batch_size,
+                              gradient_accumulation_steps=args.step_every,
+                              eval_strategy='epoch',
+                              eval_on_start=True,
+                              num_train_epochs=args.epochs,
+                              bf16=torch.cuda.is_bf16_supported(),
+                              logging_steps=args.log_every,
+                              report_to=['comet_ml'],
+                              push_to_hub=True,
+                              hub_model_id=model_id,
+                              hub_strategy='end',)
+    trainer = Trainer(model,
+                      args=targs,
+                      train_dataset=get_dataset('train', feature, tokenizer),
+                      eval_dataset=get_dataset('validation', feature, tokenizer),
+                      compute_loss_func=cls_loss,
+                      compute_metrics=cls_metrics,)
+    trainer.create_model_card(language='en',
+                              model_name=f'sbf-bart-{args.problem}',
+                              finetuned_from='facebook/bart-large-mnli',
+                              tasks='Text Classification',
+                              dataset='allenai/social-bias-frames')
+    
 
 if __name__ == '__main__':
-    if torch.cuda.is_bf16_supported():
-        torch.set_float32_matmul_precision('medium')
+    
     parser = ArgumentParser()
     parser.add_argument('-n', '--experiment_name', required=True, type=str)
     parser.add_argument('-p', '--problem', required=True, choices=['offensive', 'sex'])
-    parser.add_argument('-e', '--max_epochs', default=1, type=int)
+    parser.add_argument('-e', '--epochs', default=1, type=int)
     parser.add_argument('-g', '--step_every', default=4, type=int)
+    parser.add_argument('-l', '--log_every', default=50, type=int)
     parser.add_argument('-b', '--batch_size', default=16, type=int)
     parser.add_argument('-w', '--num_workers', default=0, type=int)
     parser.add_argument('-c', '--checkout', action="store_true")
@@ -69,7 +60,7 @@ if __name__ == '__main__':
         args.branch_name = 'main'
     print(f'EXPERIMENT NAME: {args.experiment_name}',
           f'SELECTED PROBLEM: {args.problem}',
-          f'EPOCHS: {args.max_epochs}',
+          f'EPOCHS: {args.epochs}',
           f'WORKERS: {args.num_workers}',
           f'BATCH {args.batch_size} ACCUMULATE FOR {args.step_every}',
           )
--- sbf_common.py
+++ sbf_common.py
@@ -1,19 +1,11 @@
-import comet_ml
 import torch
-import torch.nn as nn
 import torch.optim
-import datasets
-import typing
-from torch.utils.data import Dataset, DataLoader
-from transformers import (BartTokenizer,
-                          BertTokenizer,
-                          T5Tokenizer, 
-                          AutoModelForSequenceClassification,
-                          AutoConfig,
+from torch.utils.data import Dataset
+from transformers import (PreTrainedTokenizer,
+                          EvalPrediction
                           )
 from datasets import load_dataset
-from tqdm import tqdm
-from huggingface_hub import PyTorchModelHubMixin
+import numpy as np
 
 MAX_LENGTH = 256
 HF_DS = 'allenai/social_bias_frames'
@@ -27,231 +19,54 @@ tok_kwargs = {
     'add_special_tokens': True,
 }
 
-class SBFPreprocessed(Dataset):
-    def __init__(self, 
-                 hf_data: datasets.Dataset, 
-                 tok: typing.Union[BartTokenizer | BertTokenizer | T5Tokenizer],
-                 /,):
-        super().__init__()
-        self.hf_data = hf_data
-        self.tok = tok
-        self.preprocess()
-    
-    def preprocess(self):
-        def remove_blanks(row):
-            if len(row['offensiveYN']) == 0:
-                row['offensiveYN'] = '0.0'
-                # for *some* reason, there are blank entries, even though
-                # that should correspond to "not offensive"
-            if len(row['speakerMinorityYN']) == 0:
-                row['speakerMinorityYN'] = '0.0'
-                # here too ! but not in any of the other features
-            return row
-        def rebinarize_column(col: str):
-            posts = self.hf_data.unique('post')
-            mean_responses = []
-            for post in posts:
-                responses = []
-                for row in range(self.hf_data.num_rows):
-                    if row['post'] == post:
-                        responses.append([row, row[col]])
-                mean_response = torch.tensor(responses)[:, 1].squeeze().mean().round().item()
-                for response in responses:
-                    mean_responses.append(response[0], mean_response)
-            return mean_responses
-        selected_columns = [
-            'post',
-            'intentYN',
-            'offensiveYN',
-            'sexYN',
-            'speakerMinorityYN',
-        ]
-        class_encode = [
-            'intentYN',
-            'offensiveYN',
-            'sexYN',
-            'speakerMinorityYN',
-        ]
-        self.hf_data = self.hf_data.select_columns(selected_columns).map(remove_blanks)
-        for col in class_encode:
-            self.hf_data = self.hf_data.class_encode_column(col)
-            mean_responses = rebinarize_column(col)
-            for response in mean_responses:
-                self.hf_data[response[0]][col] = response[1]
-        print(self.hf_data.features)
-    
-    def __len__(self):
-        return len(self.hf_data)
-    
-    def __getitem__(self, idx) -> typing.Dict[str, torch.Tensor]:
-        row = self.hf_data[idx]
-        tok_post = self.tok.encode_plus(row['post'],
-                                        **tok_kwargs)
-        intent = torch.tensor(row['intentYN'])
-        offensive = torch.tensor(row['offensiveYN'])
-        sex = torch.tensor(row['sexYN'])
-        minority = torch.tensor(row['speakerMinorityYN'])
-        return {
-            'ids': tok_post.input_ids.view(-1),
-            'attn': tok_post.attention_mask.view(-1),
-            'intent': intent,
-            'offensive': offensive,
-            'sex': sex,
-            'minority': minority,
-        }
-        
-class SBFModel(nn.Module, PyTorchModelHubMixin):
-    def __init__(self,
-                 *,
-                 model_path: str,
-                 base_name: str):
-        super().__init__()
-        config = AutoConfig.from_pretrained(model_path,
-                                            num_labels=1)
-        self.model = AutoModelForSequenceClassification.from_pretrained(model_path, 
-                                                                        config=config,
-                                                                        ignore_mismatched_sizes=True,)
-        self.base_name = base_name
-        self.model.train()
-        
-    def forward(self, ids, attn, labels):
-        out = self.model(ids, attention_mask=attn, labels=labels)
-        return out
-    
-    def configure_optimizers(self):
-        # as a fallback
-        return torch.optim.AdamW(self.parameters(), lr=2e-4)
-        
-    def training_step(self,
-                      ids: torch.LongTensor,
-                      attn: torch.LongTensor,
-                      labels: torch.LongTensor,
-                      ) -> torch.FloatTensor:
-        out = self.forward(ids, attn, labels)
-        return out.loss
-    
-    def validation_step(self, 
-                        ids: torch.LongTensor,
-                        attn: torch.LongTensor,
-                        labels: torch.LongTensor,
-                        ) -> torch.FloatTensor:
-        out = self.forward(ids, attn, labels)
-        pred = torch.softmax(out.logits).round()
-        return pred, out.loss
-    
-    def test_step(self,
-                  batch: typing.Dict[str, torch.LongTensor],
-                  label_key: str,
-                  /,
-                  ) -> torch.FloatTensor:
-        ids = batch['ids']
-        attns = batch['attn']
-        labels = batch[label_key]
-        out = self.forward(ids, attns, labels)
-        return out.logits
-    
-    def get_loader(self, 
-                   tokenizer,
-                   /,
-                   *,
-                   split: str,
-                   shuffle: bool,
-                   **kwargs) -> DataLoader:
-        hf_data = load_dataset(HF_DS, split=split, trust_remote_code=True)
-        hf_set = SBFPreprocessed(hf_data, tokenizer)
-        hf_loader = DataLoader(hf_set, shuffle=shuffle, **kwargs)
-        return hf_loader
-    
-    def dataloaders(self, tokenizer, **kwargs):
-        return (self.train_dataloader(tokenizer, **kwargs),
-                self.val_dataloader(tokenizer, **kwargs),
-                self.test_dataloader(tokenizer, **kwargs))
-    
-    def train_dataloader(self, tokenizer, **kwargs):
-        return self.get_loader(tokenizer, split='train', shuffle=True, **kwargs)
-    
-    def val_dataloader(self, tokenizer, **kwargs):
-        return self.get_loader(tokenizer, split='validation', shuffle=False, **kwargs)
-    
-    def test_dataloader(self, tokenizer, **kwargs):
-        return self.get_loader(tokenizer, split='test', shuffle=False, **kwargs)
-    
-class SBFTrainer:
-    def __init__(self,
-                 *,
-                 max_epochs: int,
-                 step_every: int,
-                 experiment: comet_ml.Experiment,
-                 ):
-        self.max_epochs = max_epochs
-        self.step_every = step_every
-        self.experiment = experiment
-        
-    def move_all(device, *args):
-        for i in len(args):
-            args[i] = args[i].to(device)
-        return args
-        
-    def fit(self, 
-            model: SBFModel,
-            device: str,
-            *,
-            train_loader: DataLoader,
-            val_loader: DataLoader,
-            ):
-        optimizer = model.configure_optimizers()
-        label_key = model.label_key
-        model = model.to(device)
-        t = tqdm()
-        for epoch in range(self.max_epochs):
-            train_len = len(train_loader)
-            val_len = len(val_loader)
-            t.reset(total=train_len)
-            t.set_description(f'Training epoch {epoch}')
-            model.train()
-            running_loss = torch.tensor(0.0, device=device)
-            for idx, batch in enumerate(train_loader):
-                ids = batch['ids']
-                attn = batch['attn']
-                labels = batch[label_key]
-                loss = model.training_step(ids.to(device), attn.to(device), labels.to(device))
-                running_loss = running_loss + loss
-                acc_norm = torch.min(torch.tensor([4, train_len - idx])) # this accounts for number of batches not evenly dividing step_every
-                loss = loss / acc_norm
-                loss.backward()
-                
-                if (idx + 1) % self.step_every == 0 or (idx + 1) == train_len:
-                    optimizer.step()
-                    optimizer.zero_grad()
-                t.update()
-            train_loss = running_loss / train_len
-            self.experiment.log_metrics({'train_loss': train_loss.item()},
-                                        prefix=model.base_name,
-                                        epoch=epoch)
-            
-            model.eval()
-            t.reset(total=val_len)
-            t.set_description(f'Validation epoch {epoch}')
-            with torch.no_grad():
-                running_loss = torch.tensor(0.0, device=device)
-                y = []
-                yh = []
-                for batch in val_loader:
-                    ids = batch['ids']
-                    attn = batch['attn']
-                    labels = batch[label_key]
-                    preds, loss = model.validation_step(ids.to(device), attn.to(device), labels.to(device))
-                    running_loss = running_loss + loss
-                    
-                    y.append(labels)
-                    yh.append(preds)
-                    t.update()
-                y = torch.cat(y).tolist()
-                yh = torch.cat(yh).tolist()
-                val_loss = running_loss / val_len
-                self.experiment.log_metrics({'val_loss': val_loss.item()},
-                                            prefix=model.base_name,
-                                            epoch=epoch)
-                self.experiment.log_confusion_matrix(y_true=y,
-                                                     y_predicted=yh,)
-        t.clear()
+def cls_metrics(ep: EvalPrediction):
+    preds = np.rint(ep.predictions).astype(np.uint8)
+    labels = np.rint(ep.label_ids).astype(np.uint8)
+    pos_idx = preds == 1
+    neg_idx = preds == 0
+    tp = np.count_nonzero(preds[pos_idx] == labels[pos_idx]) * 1.0
+    fp = np.count_nonzero(preds[pos_idx] != labels[pos_idx]) * 1.0
+    fn = np.count_nonzero(preds[neg_idx] != labels[neg_idx]) * 1.0
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    f1 = 2 * precision * recall / (precision + recall)
+    return {
+        'precision': precision,
+        'recall': recall,
+        'f1': f1
+    } 
+
+def cls_loss(outputs, labels, *, num_items_in_batch):
+    if isinstance(outputs, dict) and "loss" not in outputs:
+        if labels is not None:
+            logits = outputs["logits"].squeeze()
+            bce = torch.nn.BCEWithLogitsLoss()
+            return bce(logits, labels)
+
+def get_dataset(split: str, feature: str, tokenizer: PreTrainedTokenizer):
+    def remove_blanks(row):
+        if len(row[feature]) == 0:
+            row[feature] = '0.0'
+            # for *some* reason, there are blank entries, even though
+            # that should correspond to '0.0'
+        return row
+    def binarize_feature(ds: Dataset):
+        posts = ds.unique('post')
+        responses = dict.fromkeys(posts, [])
+        def gather(x):
+            responses[x['post']].append(x[feature])
+        def binarize(x):
+            post = tokenizer(x['post'], **tok_kwargs)
+            mean_response = torch.tensor(responses[x['post']], dtype=torch.float32).mean().softmax(0).round().item()
+            return {
+                'input_ids': post.input_ids.view(-1),
+                'attention_mask': post.attention_mask.view(-1),
+                'labels': mean_response,
+            }
+        ds.map(gather, desc='Gathering annotator responses...')
+        return ds.map(binarize, remove_columns=['post', feature], desc='Binarizing selected feature...')
+    ds = load_dataset(HF_DS, split=split, trust_remote_code=True)
+    ds = ds.select_columns(['post', feature]).map(remove_blanks)
+    ds = ds.class_encode_column(feature)
+    ds = binarize_feature(ds)
+    return ds
