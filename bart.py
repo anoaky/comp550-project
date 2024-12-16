d72087838220fc25cce0cd334fd6072791816d37
Anokhi Suresh <17suresa@gmail.com>
2024-12-16 10:29:25 -0500

    output dir

--- bart.py
+++ bart.py
@@ -13,7 +13,7 @@ def main(args):
     tokenizer = BartTokenizer.from_pretrained('facebook/bart-large')
     model_id = f'anoaky/sbf-bart'
     feature = f'{args.problem}YN'
-    out_dir = f"model_dir"
+    out_dir = args.output_dir
     targs = TrainingArguments(output_dir=out_dir,
                               overwrite_output_dir=True,
                               run_name=args.experiment_name,
@@ -47,6 +47,7 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('-p', '--problem', required=True, choices=['offensive', 'sex'])
     parser.add_argument('-e', '--epochs', default=1, type=int)
+    parser.add_argument('-o', '--output_dir', default='outputs/model', type=str)
     args = parser.parse_args()
     args.experiment_name = f'sbf-bart-{args.problem}_{args.epochs}'
     os.environ['COMET_PROJECT_NAME'] = 'comp-550-project'
